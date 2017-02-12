0abb6005f84c4e4bc8ecb505bb2ed7bc5804709b
Peter <peter_haschke@yahoo.com>
2017-02-12 14:46:09 -0700

    Added skeleton functions.

--- background.js
+++ background.js
@@ -3,34 +3,48 @@
 // Retrieve the message sent from popup.js and alert the response.
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
-	//var outputCheckboxes = [];
+    //var outputCheckboxes = [];
     //outputCheckboxes = message.outputCheckboxes;
     //console.log("Length of checkbox array: "+outputCheckboxes.length);
     //console.log(outputCheckboxes[0]);
 
-	var blob = new Blob([message.outputData], {type: 'text/plain'});
-	console.log("Created Blob object.");
-	
-	objectURL = URL.createObjectURL(blob);
-	console.log("Made link from Blob object.");
-	
-	download(objectURL, "water_plugin_test.txt")
-	
-	
-	/**
-	 * Function that takes an objectURL and a string fileName and makes use of the ChromeAPI download to 
-	 * download the object as a file.
- 	 * @param {Object} objectURL
- 	 * @param {Object} fileName
-	 */
-	function download (objectURL, fileName){
-		chrome.downloads.download({url: objectURL, filename: fileName},
-       	function(id) {
-     	});
-	}
-	
-	
-	 
-	
-	//alert(response);
+    var blob = new Blob([message.outputData], {
+        type: 'text/plain'
+    });
+    console.log("Created Blob object.");
+
+    objectURL = URL.createObjectURL(blob);
+    console.log("Made link from Blob object.");
+
+    download(objectURL, "water_plugin_test.txt");
+    //alert(response);
 });
+
+function sortElementObjects(elementObjects) {
+
+}
+
+function createXMLFile(elementObjects) {
+
+}
+
+function createSeleniumFile(elementObjects) {
+
+}
+
+function createJSObject(elementObjects) {
+
+}
+
+
+/**
+ * Function that takes an objectURL and a string fileName and makes use of the ChromeAPI download to
+ * download the object as a file.
+ */
+function download(objectURL, fileName) {
+    chrome.downloads.download({
+            url: objectURL,
+            filename: fileName
+        },
+        function(id) {});
+}
