b47823aa3a7f599b18215f484d2507912a5bc65d
Max Wason <mw834@nau.edu>
2017-05-03 12:25:21 -0700

    The great refactoring
    Frontend

--- frontend.js
+++ frontend.js
@@ -1,32 +1,36 @@
-//For use of background page console! Usage: bkg.console.log('message!');
+//For use of background page console. Usage: bkg.console.log('message!');
 var bkg = chrome.extension.getBackgroundPage();
 
 //After the DOM is loaded, add a button listener.
-//Call the script that runs on the current tab enviroment.
+//Call the script that runs on the current tab environment.
 
+// Get the button into a variable for ease of access
+let checkPageButton = document.getElementById('checkPage');
 
-var checkPageButton = document.getElementById('checkPage');
+/**
+ * Add an event listener for clicking the button
+ */
 checkPageButton.addEventListener('click', function() {
 
     //Get user checkbox data;
-    var checkboxData = checkboxHandler();
+    let checkboxData = checkboxHandler();
 
+    // Error handling for no files selected
     if (checkboxData[0].length <= 0) {
         //Feedback that a output type must be selected, if not send user feedback to popup window.
         document.getElementById('feedback').innerHTML = "<p>You must select an output file type!</p>";
 
+    // Error handling for no elements selected
     } else if (checkboxData[1].length <= 0) {
         document.getElementById('feedback').innerHTML = "<p>You must select an element!</p>";
 
+    // No errors, pass data (selected checkboxes) to middleware
     } else {
         document.getElementById('feedback').innerHTML = "";
         chrome.tabs.query({
             active: true,
             currentWindow: true
         }, function(tabs) {
-            //chrome.tabs.executeScript(tabs[0].id, {
-            //      file: 'middleware.js'
-            //  }, function() {
             chrome.tabs.sendMessage(tabs[0].id, {
                 checkboxData: checkboxData
             }, function(response) {
@@ -37,19 +41,20 @@ checkPageButton.addEventListener('click', function() {
 
 }, false); //Button listener
 
+
 /**
  * Function to add slider functionality for options.
  */
 document.addEventListener('DOMContentLoaded', function() {
     //HTML JS for user interface//
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+    let acc = document.getElementsByClassName("accordion");
+    let i;
 
     for (i = 0; i < acc.length; i++) {
         acc[i].onclick = function() {
             this.classList.toggle("active");
-            var panel = this.nextElementSibling;
+            let panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
@@ -59,33 +64,33 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     //Check all elements
-    var getInputs = document.getElementsByClassName("element_checkbox");
-    for (var i = 0; i < getInputs.length; i++) {
+    let getInputs = document.getElementsByClassName("element_checkbox");
+    for (i = 0; i < getInputs.length; i++) {
         if (getInputs[i].type === 'checkbox') getInputs[i].checked = true;
     }
 
 }, false); //DOMContentLoaded Listener
 
 
-
 /**
  * Function to select all element attribute checkboxes.
  */
 function selectAll() {
-    var getInputs = document.getElementsByClassName("element_checkbox");
-    for (var i = 0; i < getInputs.length; i++) {
+    let getInputs = document.getElementsByClassName("element_checkbox");
+    for (let i = 0; i < getInputs.length; i++) {
         if (getInputs[i].type === 'checkbox') getInputs[i].checked = true;
     }
 }
 
+document.getElementById('selectAll').addEventListener('click', selectAll);
+
+
 /**
  * Function to deselect all element attribute checkboxes.
  */
-document.getElementById('selectAll').addEventListener('click', selectAll);
-
 function unselectAll() {
-    var getInputs = document.getElementsByClassName("element_checkbox");
-    for (var i = 0; i < getInputs.length; i++) {
+    let getInputs = document.getElementsByClassName("element_checkbox");
+    for (let i = 0; i < getInputs.length; i++) {
         if (getInputs[i].type === 'checkbox') getInputs[i].checked = false;
     }
 }
@@ -99,15 +104,16 @@ document.getElementById('unselectAll').addEventListener('click', unselectAll);
  * @returns {array} - The array of arrays of output file and element attributes selected on the GUI.
  */
 function checkboxHandler() {
-    var outputFileCheckboxes = [];
-    var elementsToBeParsedCheckboxes = [];
-    var checkboxData = [];
+    let outputFileCheckboxes = [];
+    let elementsToBeParsedCheckboxes = [];
+    let checkboxData = [];
 
-    var getFileOutputs = document.getElementsByClassName("output_checkbox");
-    var getElementTypes = document.getElementsByClassName("element_checkbox");
+    let getFileOutputs = document.getElementsByClassName("output_checkbox");
+    let getElementTypes = document.getElementsByClassName("element_checkbox");
 
     checkboxintoarray(getFileOutputs, outputFileCheckboxes);
     checkboxintoarray(getElementTypes, elementsToBeParsedCheckboxes);
+
     //Construct single array for Chrome messaging.
     checkboxData[0] = outputFileCheckboxes;
     checkboxData[1] = elementsToBeParsedCheckboxes;
@@ -116,6 +122,7 @@ function checkboxHandler() {
 
 }
 
+
 /**
  * Helper function to pull all file output types and elements meant to be parsed.
  *
@@ -123,7 +130,7 @@ function checkboxHandler() {
  * @param output - The output array of strings.
  */
 function checkboxintoarray(input, output) {
-    for (var i = 0; i < input.length; i++) {
+    for (let i = 0; i < input.length; i++) {
         if (input[i].checked) {
             output.push(input[i].id);
         }
